148be383f07baf7666b54787691427d9f8eb2c6b
srinu8688 <136572576+srinu8688@users.noreply.github.com>
2025-11-23 23:34:33 +0530

    Update CORS origin for production backend

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    origin: [process.env.PORTFOLIO_URL,"https://mern-stack-portfolio-backend-mev4.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
